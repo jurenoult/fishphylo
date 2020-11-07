9767285c24bdfcc5dab681dba7817f727a989a55
jujurenoult <jurenoult@gmail.com>
2020-11-07 19:00:40 +0100

    add disp_access function in the toolbox

--- R/tool_functions.R
+++ R/tool_functions.R
@@ -18,7 +18,7 @@ write_fasta <- function(fasta_ls,name = "fasta_seq"){
 
 #' read fasta file from output directory
 #'
-#' @param file .fas file located in the "R" directory
+#' @param file .fas file located in the data directory
 #' @param name name of output file
 #'
 #' @return an object reading fasta sequences
@@ -30,6 +30,25 @@ write_fasta <- function(fasta_ls,name = "fasta_seq"){
 #' fas_file <- read_fasta("bueniaETpomadri_COI_seqs.fasta")
 read_fasta <- function(name){
   read.fasta(here::here("data",name), as.string = TRUE)
+}
+
+
+#' display accession number and species name of a fasta file
+#'
+#' @param file an object output by function read_fasta
+#'
+#' @return display a list of accessions with the index
+#' @export
+#' @import stringr
+#'
+#' @examples
+disp_access <- function(file){
+  ls_access = ""
+  for (i in 1:length(file)){
+    access <- str_remove(word(attr(file[[i]],"Annot"), 1,3, sep=" "),"[>]")
+    ls_access=c(ls_access, paste0(i,": ", access ,"\n"))
+  }
+  cat(ls_access)
 }
 
 
---
+++ bacasable.R
@@ -0,0 +1,10 @@
+
+
+fas1 <- read_fasta("20201104_COI_fish.fasta")
+fas2 <- read_fasta("bueniaETpomadri_COI_seqs.fasta")
+
+
+
+prune_fasta <- function(file,ls_access){
+
+}
